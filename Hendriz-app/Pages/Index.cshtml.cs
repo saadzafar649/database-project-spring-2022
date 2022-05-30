be9f531b341339abc1e8e662a19ddb014b1d8d46
saadzafar649 <70218806+saadzafar649@users.noreply.github.com>
2022-05-30 19:12:03 +0500

    added slider

--- Hendriz-app/Pages/Index.cshtml.cs
+++ Hendriz-app/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace hendriz.Pages
+namespace Hendriz_app.Pages
 {
     public class IndexModel : PageModel
     {
